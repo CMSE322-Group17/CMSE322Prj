c1cef7d1b00a50af98505e155dce53c126b08d09
Emadram <refemad@outlook.com>
2025-05-11 14:49:29 +0300

    main.jsx lines: 1-1 9-11 14-16 18-22 | Type: Replacement | Reason: updated

--- campus-book-swap/src/main.jsx
+++ campus-book-swap/src/main.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
-import { CartProvider } from './contexts/CartContext';
-import './index.css';
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AuthProvider } from "./contexts/AuthContext";
+import { CartProvider } from "./contexts/CartContext";
+import { MessageProvider } from "./contexts/MessageContext";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <CartProvider>
-          <App />
+          <MessageProvider>
+            <App />
+          </MessageProvider>
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
+
