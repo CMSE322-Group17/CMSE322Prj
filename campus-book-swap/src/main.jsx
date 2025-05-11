8e20e521f3ae89416a7acd813b64481d750b9345
Emadram <refemad@outlook.com>
2025-05-11 15:12:12 +0300

    main.jsx lines: 1-1 11-10 22-21 | Type: Replacement | Reason: updating

--- campus-book-swap/src/main.jsx
+++ campus-book-swap/src/main.jsx
@@ -1,14 +1,13 @@
-// src/main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { AuthProvider } from "./contexts/AuthContext";
-import { CartProvider } from "./contexts/CartContext";
-import { MessageProvider } from "./contexts/MessageContext";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
+import { CartProvider } from './contexts/CartContext';
+import { MessageProvider } from './contexts/MessageContext';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
@@ -19,6 +18,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
-
