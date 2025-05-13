1a2a7da4e26682731af07091282cb9d618c8e719
Emadram <refemad@outlook.com>
2025-05-13 21:26:28 +0300

    Add hunk -150

--- campus-book-swap/src/pages/Home.jsx
+++ campus-book-swap/src/pages/Home.jsx
@@ -747,15 +747,11 @@ const Home = () => {
       {/* Favorite Books - Only shown when user is authenticated */}
       {isAuthenticated && (
         <FeaturedBooksRow 
-          books={booksOfWeek}
+          books={favoriteBooksList}
           title="Favorite Books"
-          icon={
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-            </svg>
-          }
-          loading={loading.booksOfWeek}
-          error={error.booksOfWeek}
+          icon={favoriteIcon}
+          loading={loading.featured}
+          error={error.featured}
         />
       )}
       
