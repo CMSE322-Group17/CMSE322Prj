18611fe923adc3b58150d23838e7a45228d2a1ce
Emadram <refemad@outlook.com>
2025-05-13 13:22:18 +0300

    Add hunk -21

--- campus-book-swap/src/components/Navbar.jsx
+++ campus-book-swap/src/components/Navbar.jsx
@@ -204,6 +204,29 @@ const NavBar = () => {
               </div>
             )}
 
+            {/* Wishlist Icon */}
+            {isAuthenticated && (
+              <Link
+                to="/wishlist"
+                className="relative ml-4 p-2 text-gray-600 hover:text-blue-600"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                </svg>
+              </Link>
+            )}
+
             {/* Messages Icon with Counter - Only for authenticated users */}
             {isAuthenticated && (
               <Link
