f47f2b91c30cc8ef2b87b5d01b0b2c0d482522be
Emadram <refemad@outlook.com>
2025-05-13 13:22:19 +0300

    Add hunk -48

--- campus-book-swap/src/pages/Home.jsx
+++ campus-book-swap/src/pages/Home.jsx
@@ -737,7 +737,7 @@ const Home = () => {
           
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
             {books.map(book => (
-              <FeaturedBook key={book.id} book={book} />
+              <BookCard key={book.id} book={book} onClick={() => setSelectedBook(book)} />
             ))}
           </div>
         </div>
