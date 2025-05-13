3f4baddbef6221d412a48b89cd1e9da7dfe071da
Emadram <refemad@outlook.com>
2025-05-13 21:26:25 +0300

    Add hunk -104

--- campus-book-swap/src/pages/Dashboard.jsx
+++ campus-book-swap/src/pages/Dashboard.jsx
@@ -243,7 +243,6 @@ const Dashboard = () => {
         `${import.meta.env.VITE_API_URL}/api/swap-offers?filters[$or][0][buyerId][$eq]=${user.id}&filters[$or][1][sellerId][$eq]=${user.id}&filters[status][$eq]=completed&sort[0]=timestamp:desc&populate=*`
       );
       
-      // Process sales data
       const sales = salesResponse.data.data?.map(order => ({
         id: `sale-${order.id}`,
         type: 'purchase',
