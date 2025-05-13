9df8be36971f32c7cfbba42f2c013c34c3eb470a
Emadram <refemad@outlook.com>
2025-05-13 13:22:18 +0300

    Add hunk -40

--- campus-book-swap/src/pages/Dashboard.jsx
+++ campus-book-swap/src/pages/Dashboard.jsx
@@ -332,8 +332,6 @@ const Dashboard = () => {
         `${import.meta.env.VITE_API_URL}/api/swap-offers?filters[$or][0][buyerId][$eq]=${user.id}&filters[$or][1][sellerId][$eq]=${user.id}&filters[status][$eq]=pending&pagination[pageSize]=1&pagination[page]=1`
       );
       
-      // Borrow functionality removed
-      
       // Get pending purchase requests count
       const pendingPurchasesResponse = await authAxios.get(
         `${import.meta.env.VITE_API_URL}/api/messages?filters[receiver][id][$eq]=${user.id}&filters[messageType][$eq]=purchase_request&filters[requestStatus][$eq]=pending&pagination[pageSize]=1&pagination[page]=1`
