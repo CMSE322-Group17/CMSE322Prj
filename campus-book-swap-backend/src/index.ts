8ced2bd024760752acea116b53a649acfd7d4082
Emadram <refemad@outlook.com>
2025-05-14 23:10:00 +0300

    Fixed Websockets - Transaction Left - Message Left Stage hunk #243

--- campus-book-swap-backend/src/index.ts
+++ campus-book-swap-backend/src/index.ts
@@ -7,7 +7,7 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register() {},
 
   /**
    * An asynchronous bootstrap function that runs before
